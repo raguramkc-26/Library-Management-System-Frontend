a8ffc77094ce57d4dde3dcaf68a0acf2b827499b
raguramkc-26 <raguramkc038@gmail.com>
2026-04-23 18:35:52 +0530

    issues resolved

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -17,8 +17,6 @@ export const AuthProvider = ({ children }) => {
       try {
         const res = await instance.get("/auth/getMe");
         const userData = res.data?.user;
-        console.log("GETME RESPONSE:", res.data);
-        console.log("EXTRACTED USER:", userData);
         setUser(userData || null); 
       } catch (err) {
         console.log("GETME ERROR:", err.response?.data || err.message);
