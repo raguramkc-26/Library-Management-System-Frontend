14d5249a6eb652b2bbce30cd48eba25f8866326b
raguramkc-26 <raguramkc038@gmail.com>
2026-05-01 00:13:10 +0530

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -7,4 +8,4 @@ export default {
     extend: {},
   },
   plugins: [],
-}
+};
